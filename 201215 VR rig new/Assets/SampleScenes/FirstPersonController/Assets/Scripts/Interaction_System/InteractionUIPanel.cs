ee90ab6eb5237943a70cb55deb91d7229dfe74db
leometcalf <76454215+leometcalf@users.noreply.github.com>
2020-12-22 13:33:58 +0000

    added project

---
+++ 201215 VR rig new/Assets/SampleScenes/FirstPersonController/Assets/Scripts/Interaction_System/InteractionUIPanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace VHS
+{        
+    public class InteractionUIPanel : MonoBehaviour
+    {
+        [SerializeField] private Image progressBar;
+
+        public InteractionUIPanel(TextMeshProUGUI tooltipText) => this.tooltipText = tooltipText != null ? tooltipText : throw new System.ArgumentNullException(nameof(tooltipText));
+
+        [SerializeField] private TextMeshProUGUI tooltipText;
+
+        public void SetTooltip(string tooltip)
+        {
+            NewMethod(tooltip);
+        }
+
+        private void NewMethod(string tooltip)
+        {
+            tooltipText.SetText(tooltip);
+        }
+
+        public void UpdateProgressBar(float fillAmount)
+        {
+            progressBar.fillAmount = fillAmount;
+        }
+
+        public void ResetUI()
+        {
+            progressBar.fillAmount = 0f;
+            tooltipText.SetText("");
+        }
+    }
+}
